c93c29b7e63928444352c5bfaac3a93a8967a47b
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-04-11 10:13:39 +0100

    Ausgabe jetzt als stehender Vektor (neue Konvention)

--- transformation/r2rotvec.m
+++ transformation/r2rotvec.m
@@ -1,17 +1,28 @@
-% Konvertiere eine Rotationsdarstellung von Rotationsmatrix zu Rotationsvektor (aus
-% [ZupanSaj2011])
+% Konvertiere eine Rotationsdarstellung von Rotationsmatrix zu Rotationsvektor 
+% (aus [ZupanSaj2011])
+% 
+% Eingabe:
+% R [3x3]
+%   Rotationsmatrix
+% Ausgabe:
+% rotvec [3x1]
+%   Rotationsvektor (Achse x Winkel aus Achse-Winkel-Darstellung
 % 
 % Quelle:
 % [ZupanSaj2011] Integrating rotation from angular velocity , Kap. 3.1
 
 % Moritz Schappler, schappler@irt.uni-hannover.de, 2016-02
-% (c) Institut für Regelungstechnik, Universität Hannover
+% (C) Institut für Regelungstechnik, Universität Hannover
 
 function rotvec = r2rotvec(R)
+
+%#codegen
+assert(isa(R,'double') && isreal(R) && all(size(R) == [3 3]), ...
+	'r2rotvec: R = [3x3] double');   
 
 [theta, n] = r2angvec(R); % Drehwinkel und Drehachse (Einheitsvektor)
 if theta == 0
   rotvec = [0;0;0];
 else
-  rotvec = n' * theta;
+  rotvec = n * theta;
 end
