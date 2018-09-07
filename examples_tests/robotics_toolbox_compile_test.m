4670ea5b1488a9097ca2d4841eae5cffd231e5ca
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-09-07 13:34:06 +0100

    Testskript angepasst zur Verwendung unter Windows

--- examples_tests/robotics_toolbox_compile_test.m
+++ examples_tests/robotics_toolbox_compile_test.m
@@ -7,14 +7,24 @@ clear
 clc
 
 tb_path = fileparts(which('robotics_toolbox_path_init.m'));
+
 pp = split(path(),':');
 for p = pp'
-  % Kompiliere nur Unterordner dieser Toolbox, die auch im Pfad sind.
-  if contains(p, tb_path)
-    mex_all_matlabfcn_in_dir(p{1}, 1);
+  if ispc()
+    % Unter Windows wird das Laufwerk hintenangestellt (mit ";C"),
+    % in der Absoluten Pfadangabe wird das Laufwerk am Anfang geschrieben ("C:"}
+    p_abs = sprintf('%s:%s', p{1}(end), p{1}(1:end-2)); % korrigiere Pfadvariable
+  else
+    % Normales Betriebssystem, kein Windows. Keine Korrektur notwendig.
+    p_abs = p{1};
   end
-  % Jetzt kommt nur noch das Matlab-Stammverzeichnis
-  if contains(p, matlabroot)
+  % Kompiliere nur Unterordner dieser Toolbox, die auch im Pfad sind.
+  if contains(p_abs, tb_path)
+    mex_all_matlabfcn_in_dir(p_abs, 1);
+  end
+  % Jetzt kommt nur noch das Matlab-Stammverzeichnis (steht immer am Ende
+  % des Pfades
+  if contains(p_abs, matlabroot)
     break;
   end
 end
