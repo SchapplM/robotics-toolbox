22e1200a5d918680224ee7d7287b9f96e80ae523
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-05-14 08:29:41 +0100

    [ParRob] Methode zur Berechnung aller Schnittkräfte der PKM

---
+++ matlab_class/@ParRob/internforce.m
@@ -0,0 +1,78 @@
+% Interne Schnittkräfte der PKM basierend auf inverser Dynamik
+% 
+% Eingabe:
+% q, qD, qDD
+%   Gelenkkoordinaten aller Gelenke aller Beinketten der PKM
+% tauA
+%   Antriebskräfte der PKM (nur bezogen auf aktuierte Gelenke)
+% 
+% Ausgabe:
+% w_B [6xNLEG]
+%   Schnittkräfte und -momente in allen Plattform-Koppelpunkten
+% w_all_linkframe [6xNLEGJxNLEG]
+%   Alle Schnittkräfte in allen Gelenken der PKM. Ausgedrückt im Körper-KS
+% w_all_baseframe
+%   ... ausgedrückt im Basis-KS der PKM.
+% 
+% Quellen:
+% Aufzeichnungen Schappler, 9.5.19
+
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2019-05
+% (C) Institut für Mechatronische Systeme, Universität Hannover
+
+function [w_B, w_all_linkframe, w_all_baseframe] = internforce(RP, q, qD, qDD, tauA)
+
+% Ausgabevariablen initialisieren
+w_B = NaN(6, RP.NLEG);
+w_all_linkframe = NaN(6, RP.Leg(1).NL, RP.NLEG); % TODO: Aktuell nur symmetrische PKM
+w_all_baseframe = w_all_linkframe;
+
+% Berechne die Schnittkräfte. Siehe Aufzeichnungen Schappler, 9.5.19
+for j = 1:RP.NLEG % Für alle Beinketten
+  % Initialisierung der aktuellen Beinkette
+  q_j = q(RP.I1J_LEG(j):RP.I2J_LEG(j));
+  qD_j = qD(RP.I1J_LEG(j):RP.I2J_LEG(j));
+  qDD_j = qDD(RP.I1J_LEG(j):RP.I2J_LEG(j));
+  % Gelenkmomente aufgrund interner Dynamik
+  tau_j = RP.Leg(j).invdyn(q_j, qD_j, qDD_j);
+  % Antriebsmomente dieses Beins (passive sind Null)
+  tau_m_j = zeros(RP.Leg(j).NQJ,1);
+  tau_m_j(RP.I_qa(RP.I1J_LEG(j):RP.I2J_LEG(j))) = tauA(j); % TODO: Aktuell nur ein Antrieb pro Bein
+  R_0_0j = RP.Leg(j).T_W_0(1:3,1:3); % Rotation PKM-Basis - Beinkette-Basis
+
+  % Bein-Jacobi-Matrix für Koppelpunkt. Im PKM-Basis-KS
+  J_j_0 = [R_0_0j, zeros(3,3); zeros(3,3), R_0_0j] * RP.Leg(1).jacobig(q_j);
+  % Kraft, mit der die Plattform auf die Beinketten drückt (PKM-Basis-KS)
+  % Die externe Kraft verursacht zusammen mit den Antriebskräften das
+  % Bewegungsverhalten (q,qD,qDD) der Beinkette
+  FB_j_0 = (J_j_0') \ (tau_j - tau_m_j);
+
+  % Berechne die Schnittkräfte im Bein.
+  % Modell-Annahme: Bein ist freistehende serielle Kette, an der vorne die
+  % Schnittkraft mit der Plattform als externe Kraft angreift.
+  % Kraft in Bein-Basis-KS umrechnen
+  F_B_j_0j = rotate_wrench(FB_j_0, R_0_0j');
+  % Schnittkräfte des Beins im Bein-KS berechnen ("l"=Linkframe)
+  W_j_l_ext = RP.Leg(j).internforce_ext(q_j, F_B_j_0j, RP.Leg(j).NL-1, zeros(3,1));
+  % Schnittkräfte aufgrund der internen Kräfte
+  W_j_l_int = RP.Leg(j).internforce(q_j, qD_j, qDD_j);
+
+  % Gesamte Schnittkräfte: Differenz entspricht Schnittkraft im Gelenk.
+  % Je nach Gelenktyp in der Struktur aufgefangen oder in Richtung des
+  % Gelenk-FG
+  W_j_l = W_j_l_ext - W_j_l_int;
+
+  % Schnittkräfte wieder in Basis-KS zurückrechnen (sind vorher im
+  % Körper-KS)
+  Tc_j_0j = RP.Leg(j).fkine(q_j);
+  W_j_0 = W_j_l*NaN;
+  for k = 1:size(W_j_l,2)
+    R_0j_l = Tc_j_0j(1:3,1:3,k);
+    W_j_0(:,k) = rotate_wrench(W_j_l(:,k), R_0_0j*R_0j_l);
+  end
+
+  % Abspeichern der Ergebnisse
+  w_B(:,j) = FB_j_0;
+  w_all_linkframe(:,:,j) = W_j_l;
+  w_all_baseframe(:,:,j) = W_j_0;
+end
