401443d44c6a48ed9e69b71c2f389774b599d893
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-06-14 08:45:46 +0100

    Optimierungs-Zielfunktion angepasst: Bei Gelenkwinkel-Grenzverletzung wird der Gradient zu Null gesetzt

--- kinematics/invkin_optimcrit_limits2.m
+++ kinematics/invkin_optimcrit_limits2.m
@@ -20,12 +20,25 @@ function [h, hdq] = invkin_optimcrit_limits2(q, qlim)
 
 n = length(q);
 h = 0;
-hdq = NaN(1,n);
+hdq = zeros(1,n);
+
+% Der Gradient darf nur für Bereiche definiert sein, wo die Winkel in ihrem
+% Toleranzbereich liegen. Ansonsten können die Gelenkwinkel nicht mehr
+% zurückkehren
+I_viol = q<qlim(:,1) | q>qlim(:,2);
+
 for i = 1:n
-  % [ZhuQuCaoYan2013], Gl. 4
-  h = h + (qlim(i,2)-qlim(i,1))^2/(8*n) * (1/(q(i)-qlim(i,1))^2+...
-                                           1/(q(i)-qlim(i,2))^2);
-  % Gradient: Berechnung Schappler vom 01.06.2019
-  hdq(i) = -(qlim(i,2)-qlim(i,1))/(4*n) * (1/(q(i)-qlim(i,1))^3+...
-                                           1/(q(i)-qlim(i,2))^3);
+  if ~I_viol(i)
+    % [ZhuQuCaoYan2013], Gl. 4
+    h = h + (qlim(i,2)-qlim(i,1))^2/(8*n) * (1/(q(i)-qlim(i,1))^2+...
+                                             1/(q(i)-qlim(i,2))^2);
+    % Gradient: Berechnung Schappler vom 01.06.2019
+    hdq(i) = -(qlim(i,2)-qlim(i,1))/(4*n) * (1/(q(i)-qlim(i,1))^3+...
+                                             1/(q(i)-qlim(i,2))^3);
+  else
+    
+    h = Inf;
+    % Keine Berechnung von hdq: Bleibt Null
+  end
 end
+
