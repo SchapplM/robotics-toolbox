e0757f111bde79989f9cd3432afa22c7a338fa88
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-01-25 10:01:58 +0100

    [Matlab-Klasse] Fehler in Steifigkeitsmodell entfernt. Kommentare ergänzt. Squashed commit of the following:
    commit 78002e807417acdad56fe03f1e92b123bb6ff838
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Sat Jan 25 10:00:35 2020 +0100
    
        [Matlab-Klasse/Steifigkeit] Plausibilitätsprüfungen zur Fehlersuche entfernt
    
    commit 77c795e482166d6a2b79445c1e7891115edffa92
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Sat Jan 25 09:59:53 2020 +0100
    
        [SerRob] Fehler in Steifigkeitsmodell bzgl passiver Schubgelenke korrigiert
    
    commit e807ca782463cb5d14d643795ea37c2d0637a0ab
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Fri Jan 24 15:06:25 2020 +0100
    
        [SerRob] Kommentare für Steifigkeitsberechnung aus MA Zhao
    
    commit eec759752970b22635c7d5c6e9160f0a77886f33
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Fri Jan 24 14:46:52 2020 +0100
    
        [SerRob] Fehler in Steifigkeitsmodell korrigiert.
    
    commit 07ef9a7ea1a16dfc510b94b35c13af751cd3607d
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Thu Jan 23 16:44:43 2020 +0100
    
        [SerRob] Falsche Eigenwertprüfung entfernt.
    
    commit b235abff5237ecb1217f2d4240bb0fd162453301
    Author: Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
    Date:   Wed Jan 22 18:21:20 2020 +0100
    
        [Matlab-Klasse] Debug-Einstellungen für Steifigkeitsberechnung

--- matlab_class/@SerRob/stiffness.m
+++ matlab_class/@SerRob/stiffness.m
@@ -23,6 +23,9 @@
 % Quellen:
 % [Zhao2020] Modellierung und Maßsynthese serieller und paralleler Roboter
 % hinsichtlich der strukturellen Steifigkeit (Masterarbeit)
+% [Krefft2006] Aufgabenangepasste Optimierung von Parallelstrukturen für
+% Maschinen in der Produktionstechnik
+% [Zhang2009] Parallel Robotic Machine Tools
 % [Klimchik2011] Enhanced stiffness modeling of serial and parallel
 % manipulators for robotic-based processing of high performance materials
 
@@ -51,21 +54,21 @@ L_max=R.reach();
 %% Nachgiebigkeit Datenbank für Schub und Drehgelenke
 M_G = m_L*9.8*L_max/2; %Moment aus Selbstgewicht
 if R.islegchain
-  S = 14.7/(1.5*M_G); %Skalierungsfaktor (Norm: M = 12*9.8/6*0.5+2*9.8*0.25) . TODO: Quelle
+  S = 14.7/(1.5*M_G); %Skalierungsfaktor (Norm: M = 12*9.8/6*0.5+2*9.8*0.25 nach ABB IRB 360).
 else
   S = 11600/(2*M_G); %Skalierungsfaktor (Nach KUKA 240-2 Datenblatt: M = 240*9.8*3+400*9.8*1.5)
 end
 if R.islegchain
-  % Für Drehgelenk (Aktiv). Nachgiebigkeit Daten aus [Klimchik2011] P52.
+  % Für Drehgelenk (Aktiv). Nachgiebigkeit Daten aus [Zhang2009] S.79; [Klimchik2011] S.52.
   Nd_a = S*diag([0.0000167, 0.0000167, 0.000022, 0.00002, 0.00002, 0.0000333]);
-  % Für Kugelgelenk (Passiv). TODO: Quelle
+  % Für Kugelgelenk (Passiv). aus ANSYS Simulation (Modell in ANSYS zu klein, wird mit 0,1 multipliziert)
   NK_p = S*diag([3.33E-7, 1E-7, 1E-7, 1e9, 1e9, 1e9]); 
 
-  % Für Uni-Gelenk (Passiv). TODO: Quelle
+  % Für Kardan-Gelenk (Passiv). aus ANSYS Simulation (Wie Kugelgelenk)
   NU_p = S*diag([1.45E-6, 3E-5, 3E-5, 0.0022, 1e9, 1e9]); 
 else % Serieller Roboter
   % Für Drehgelenke
-  Nd_a = S*diag([1.428E-8, 1.428E-8, 2.2E-8, 2E-8, 2E-8, 1.52E-7]); %Nachgiebigkeit Daten aus [HAL] P52
+  Nd_a = S*diag([1.428E-8, 1.428E-8, 2.2E-8, 2E-8, 2E-8, 1.52E-7]);%Nachgiebigkeit Daten aus [Klimchik2011] P52.
   NK_p = NaN(6,6); % In seriellen Robotern kann es keine Kugelgelenke geben
   NU_p = NaN(6,6);
 end
@@ -73,7 +76,7 @@ end
 E = R.DesPar.material(2); % E-Modul; z.B. Aluminium 70GPa
 G = R.DesPar.material(3); % Schubmodul; z.B. Aluminium 25.5GPa
 
-%% Geometrische Jacobi-Matrix
+%% Vollständige Geometrische Jacobi-Matrix
 Jg_C = R.jacobig_cutforce(q, uint8(NJ), zeros(3,1));
 
 %% Steifigkeitsmatrix für alle Glieder berechen
@@ -89,25 +92,26 @@ for i = 1:NJ
   Iy = pi*(D_r^4-d_r^4)/64; % axiale Flächenträgheitsmoment des Gliedes um y und z achsen
   Iz = Iy;
   Ip = pi*(D_r^4-d_r^4)/32; % polare Flächenträgheitsmoment des Gliedes
-  R_L = roty(atan2(di,ai))*rotx(alphai)';
+  R_L = roty(atan2(di,ai))*rotx(alphai)'; %Rotation von L-Glied zum geraden Glied
 
+  % aus [Krefft2006] S. Gl.96, S. 56
   kx_Fx = Li/(E*pi*(D_r^2-d_r^2)/4);
-  ky_Fy = Li^3/(3*E*Iy);
+  ky_Fy = Li^3/(3*E*Iy); % [Krefft2006]/(3.97)
   kz_Fz = Li^3/(3*E*Iz);
   ky_Mz = Li^2/(2*E*Iz);
   kz_My = -Li^2/(2*E*Iy);
-  kdx_Mx = Li/(G*Ip);
+  kdx_Mx = Li/(G*Ip); % [Krefft2006]/(3.98)
   kdy_My = Li/(E*Iy);
   kdz_Mz = Li/(E*Iz);
   kdy_Fz = -Li^2/(2*E*Iy);
   kdz_Fy = Li^2/(2*E*Iz);
-
-  % TODO: Warum Überlagerung und Rotation um 90°?
+  % Zusammensetzen von [Krefft2006] S. Gl.96
   ng1_1 = diag([kx_Fx, ky_Fy, kz_Fz, kdx_Mx, kdy_My, kdz_Mz]);
   ng1_2 = rot90(diag([kdz_Fy, kdy_Fz, 0, kz_My, ky_Mz],-1),1);
   ngL = ng1_1 + ng1_2; % Nachgiebigkeit des Gliedes im lokalen Koordinatensystem
 
   Jg = Jg_C(:,(6*i+1):(6*i+6));
+  %Rotation der Steifigkeitsmatrix aus [Klim2011]
   ngL_loc = [R_L, zeros(3); zeros(3), R_L] * ngL * [R_L', zeros(3); zeros(3), R_L'];
   ngL_c_glob = Jg*ngL_loc*transpose(Jg);
   % Nachgiebigkeit der seriellen Struktur ist summe der Nachgiebigkeiten
@@ -129,7 +133,7 @@ for j = 1:NJ
     NGJ_c_glob_sum = NGJ_c_glob_sum + ngJ_c_glob;
   elseif R.DesPar.joint_type(j) == 0 % sigma(j) == 0 %Ist einzel Drehgelenk
     ngJ_c = Nd_a; %Steifigkeit-Datenbank für einzeln Drehgelenk benutzen
-    if I_qa(j) == 0 %Passiv Gelenk
+    if I_qa(j) == 0 %Passives Gelenk
       ngJ_c(6,6) = 1E9; % Um z-Achse frei zu drehen
     end
     ngJ_c_glob = Jg*ngJ_c*transpose(Jg);
@@ -137,7 +141,9 @@ for j = 1:NJ
   elseif any(R.DesPar.joint_type(j) == [1 4 5])  % Ist Schubgelenk
     q_min = R.qlim(j,1);
     q_max = R.qlim(j,2);
-    R_s = roty(pi/2);
+    R_s = roty(pi/2); % Rotationsmatrix des Schubgelenkes. In der Form aus [Krefft2006]
+    % liegt die x-Achse entlang des Gliedes, aber für Schubgelenk muss die
+    % z-Achse entlang des Gelenkes liegen
 
     L_0 = max(abs(q_min),abs(q_max))/4; %L - Länge der Mutter (Null Position)
 
@@ -145,30 +151,33 @@ for j = 1:NJ
     Iy = pi*D_r^4/64;
     Iz = Iy;
     Ip = 2*Iy;
-    Iy_r = pi*d_r^4/64; 
-    Iz_r = Iy;
+    Iy_r = pi*d_r^4/64; % TODO: Wird das bei Vollmaterial Null?
+    Iz_r = Iy_r;
     Ip_r = 2*Iy_r; 
 
-    % TODO: Woher kommt die Zahl?
+    % Antriebsnachgiebigkeit des Schubgelenkes aus [Zhang2009] S.79
     kx_Fx = S*(1.613e-6); %Actuator Steifigkeit des Schubgelenkes
-    ky_Fy = L_0^3/(3*E*Iy)+L_s^3/(3*E*Iy_r);
-    kz_Fz = L_0^3/(3*E*Iz)+L_s^3/(3*E*Iz_r);
-    ky_Mz = L_0^2/(2*E*Iz)+L_s^2/(2*E*Iz_r);
-    kz_My = -L_0^2/(2*E*Iy)-L_s^2/(2*E*Iy_r);
-    kdx_Mx = L_0/(G*Ip)+L_s/(G*Ip_r);
-    kdy_My = L_0/(E*Iy)+L_s/(E*Iy_r);
-    kdz_Mz = L_0/(E*Iz)+L_s/(E*Iz_r);
-    kdy_Fz = -L_0^2/(2*E*Iy)-L_s^2/(2*E*Iy_r);
-    kdz_Fy = L_0^2/(2*E*Iz)+L_s^2/(2*E*Iz_r);
-
-    ng2_1 = diag([kx_Fx, ky_Fy, kz_Fz, kdx_Mx, kdy_My, kdz_Mz]);
-    ng2_2 = rot90(diag([kdz_Fy, kdy_Fz, 0, kz_My, ky_Mz],-1),1);
+    ky_Fy = L_0^3/(3*E*Iy) + L_s^3/(3*E*Iy_r);
+    kz_Fz = L_0^3/(3*E*Iz) + L_s^3/(3*E*Iz_r);
+    ky_Mz = L_0^2/(2*E*Iz) + L_s^2/(2*E*Iz_r);
+    kz_My = -L_0^2/(2*E*Iy) - L_s^2/(2*E*Iy_r);
+    kdx_Mx = L_0/(G*Ip) + L_s/(G*Ip_r);
+    kdy_My = L_0/(E*Iy) + L_s/(E*Iy_r);
+    kdz_Mz = L_0/(E*Iz) + L_s/(E*Iz_r);
+    kdy_Fz = -L_0^2/(2*E*Iy) - L_s^2/(2*E*Iy_r);
+    kdz_Fy = L_0^2/(2*E*Iz) + L_s^2/(2*E*Iz_r);
+    % [Krefft2006] Gl. 3.96 
+    ng2_1 = diag([kx_Fx, ky_Fy, kz_Fz, kdx_Mx, kdy_My, kdz_Mz]); % Hauptdiagonale
+    ng2_2 = rot90(diag([kdz_Fy, kdy_Fz, 0, kz_My, ky_Mz],-1),1); % Nebendiagonale
     ng2 = ng2_1 + ng2_2;
-    Ns = [R_s, zeros(3); zeros(3), R_s]*ng2*[R_s, zeros(3); zeros(3), R_s]';
+    % Inf-Terme entfernen, soweit vorhanden. Dadurch wird die entstehende
+    % Matrix singulär. Ersetze durch sehr hohen Zahlenwert.
+    ng2(isinf(ng2)) = 1e10 * sign(ng2(isinf(ng2)));
+    Ns = [R_s, zeros(3); zeros(3), R_s]*ng2*[R_s, zeros(3); zeros(3), R_s]'; % [Klim2011]
     ngJ_c = Ns;       % Datenbank für Schubgelenke benutzen
-    if I_qa(j) == 0 % Passiv Gelenk (meistens kein passives Schubgelenk)
-      ngJ_c = Ns; % Steifigkeit-Datenbank für einzeln Schubgelenk benutzen
-      ngJ_c(1,1) = 1E9; % Entlang der x-Achse frei zu schieben. TODO: Warum x und nicht z?
+    if I_qa(j) == 0 % Passives Gelenk (unüblich bei real existierenden Systemen)
+      ngJ_c = Ns; % Steifigkeit-Datenbank für einzelnes Schubgelenk benutzen
+      ngJ_c(3,3) = 1E9; % Entlang der z-Achse frei verschiebbar
     end
     ngJ_c_glob = Jg*ngJ_c*Jg';
     % Nachgiebigkeit der seriellen Struktur ist summe der Nachgiebigkeiten
