496662c685f7b832e505bc20b175424c6249f272
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-05-13 12:11:04 +0100

    [ParRob] Anpassung an KS bei hybriden Beinketten

--- matlab_class/@ParRob/fkine_legs.m
+++ matlab_class/@ParRob/fkine_legs.m
@@ -38,19 +38,21 @@ for iLeg = 1:Rob.NLEG
   phi_0_Ai = Rob.Leg(iLeg).phi_W_0;
   R_0_0i = eul2r(phi_0_Ai, Rob.Leg(iLeg).phiconv_W_0);
   T_0_0i = transl(r_0_0_Ai) * r2t(R_0_0i);
-  
+  % Alle Körper-KS der Beinkette
   Tc_0i = Rob.Leg(iLeg).fkine(qs);
-  Tc_0 = Tc_0i;
-  for i = 1:Rob.Leg(iLeg).NL
+  Tc_0 = Tc_0i(:,:,1:Rob.Leg(iLeg).NL); % Nehme nur die KS die zu in NL gezählten Körpern gehören
+  for i = 1:size(Tc_0,3)
     Tc_0(:,:,i) = T_0_0i * Tc_0i(:,:,i);
   end
-  Tc_0(:,:,end+1) = Tc_0(:,:,end) * Rob.Leg(iLeg).T_N_E; %#ok<AGROW>
+  % Zusätzliches KS für virtuellen Endeffektor der Beinkette
+  Tc_0(:,:,end+1) = Tc_0(:,:,Rob.Leg(iLeg).I_EElink+1) * Rob.Leg(iLeg).T_N_E; %#ok<AGROW>
+  % In Ausgabevariable eintragen
   J1 = J2+1;
   J2 = J1+Rob.Leg(iLeg).NL-1+1;
-  Tc_Lges(:,:,J1:J2) = Tc_0(:,:,1:(Rob.Leg(iLeg).NL+1));
+  Tc_Lges(:,:,J1:J2) = Tc_0(:,:,[1:Rob.Leg(iLeg).NL, end]);
 end
 
-%% Trnasformation ins Welt-KS
+%% Transformation ins Welt-KS
 if nargout == 2
   Tc_Lges_W = NaN(4,4,size(Tc_Lges,3));
   for i = 1:size(Tc_Lges,3)
