671ee1ea38e5f53c8b52096d29b1ac318c0eb682
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2022-01-30 10:08:07 +0100

    [SerRob] Fehlende Fälle für Variableninitialisierung ergänzt

--- matlab_class/@SerRob/SerRob.m
+++ matlab_class/@SerRob/SerRob.m
@@ -1209,6 +1209,18 @@ classdef SerRob < RobBase
         R.I_constr_red = [1 2 4 5 6];
         R.I_constr_t_red = [1 2];
         R.I_constr_r_red = [3 4 5];
+      elseif all(I_EE_Task == [1 1 1 0 0 0]) % 3T3R
+        if all(R.I_EE == [1 1 1 0 0 1]) || all(R.I_EE == [1 1 1 0 0 0])
+          % Aufgabenredundanz oder Übereinstimmung der FG
+          R.I_constr_red = 1:3;
+          R.I_constr_t_red = R.I_constr_t;
+          R.I_constr_r_red = [];
+        else
+          % Allgemeiner Fall. Betrachte ohne Redundanz
+          R.I_constr_red = 1:6;
+          R.I_constr_t_red = R.I_constr_t;
+          R.I_constr_r_red = R.I_constr_r;
+        end
       else
         error('Case not defined');
       end
